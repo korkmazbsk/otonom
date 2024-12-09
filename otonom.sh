9b535941c45d3c8dd4e13ebaf63356386d2706f5
Bayram Korkmaz <70453868+korkmazbsk@users.noreply.github.com>
2024-12-10 01:55:34 +0300

    Update otonom.sh

--- otonom.sh
+++ otonom.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # On-demand instance türleri
-instance_types_old=("r7a.16xlarge" "m7a.16xlarge" "c7a.16xlarge")
-instance_types_new=("r7i.16xlarge" "m7i.16xlarge" "c7i.16xlarge")
+instance_types_old=("c7a.16xlarge")
+instance_types_new=("m7i.16xlarge" "c7i.16xlarge")
 
 # Eski bölgeler
 regions_old=("us-east-1" "us-east-2" "ap-northeast-1" "eu-central-1" "eu-west-1" "eu-north-1" "us-west-2")
